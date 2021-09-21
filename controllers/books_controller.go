1106ea217f0d5b211973c66a2aa1546e180a3cb8
Francisco Pessoa <franciscoaap7@gmail.com>
2021-09-21 15:30:37 -0300

    add controllers/routes and persist data at postgres database

--- controllers/books_controller.go
+++ controllers/books_controller.go
@@ -1,9 +1,121 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"strconv"
+
+	"github.com/franciscopessoa/books-api/database"
+	"github.com/franciscopessoa/books-api/database/models"
+	"github.com/gin-gonic/gin"
+)
 
 func ShowBook(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"value": "ok",
-	})
+	id := c.Param("id")
+	newId, err := strconv.Atoi(id)
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "ID has to be integer",
+		})
+		return
+	}
+
+	var book models.Book
+
+	db := database.GetInstance()
+	err = db.First(&book, newId).Error
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "cannot find book: " + err.Error(),
+		})
+		return
+	}
+
+	c.JSON(200, book)
+}
+
+func ShowBooks(c *gin.Context) {
+	db := database.GetInstance()
+	var p []models.Book
+	err := db.Find(&p).Error
+
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "cannot find product by id: " + err.Error(),
+		})
+		return
+	}
+
+	c.JSON(200, p)
+}
+
+func CreateBook(c *gin.Context) {
+	db := database.GetInstance()
+
+	var p models.Book
+
+	err := c.ShouldBindJSON(&p)
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "cannot bind JSON: " + err.Error(),
+		})
+		return
+	}
+
+	err = db.Create(&p).Error
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "cannot create book: " + err.Error(),
+		})
+		return
+	}
+
+	c.JSON(200, p)
+}
+
+func DeleteBook(c *gin.Context) {
+	id := c.Param("id")
+	newid, err := strconv.Atoi(id)
+
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "ID has to be integer",
+		})
+		return
+	}
+
+	db := database.GetInstance()
+
+	err = db.Delete(&models.Book{}, newid).Error
+
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "cannot delete book: " + err.Error(),
+		})
+		return
+	}
+
+	c.Status(204)
+}
+
+func UpdateBook(c *gin.Context) {
+	db := database.GetInstance()
+
+	var p models.Book
+
+	err := c.ShouldBindJSON(&p)
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "cannot bind JSON: " + err.Error(),
+		})
+		return
+	}
+
+	err = db.Save(&p).Error
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "cannot create book: " + err.Error(),
+		})
+		return
+	}
+
+	c.JSON(200, p)
 }
--- database/models/book.go
+++ database/models/book.go
@@ -10,7 +10,7 @@ type Book struct {
 	ID          uint           `json:"id" gorm:"primaryKey"`
 	Name        string         `json:"name"`
 	Description string         `json:"description"`
-	MediumPrice string         `json:"medium_price"`
+	MediumPrice float32        `json:"medium_price"`
 	Author      string         `json:"author"`
 	ImageURL    string         `json:"img_url"`
 	CreatedAt   time.Time      `json:"created"`
